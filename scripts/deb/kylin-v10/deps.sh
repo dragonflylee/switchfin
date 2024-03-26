60b90b46175895d3e76c1d3e6a143e4d04d7342f
dragonflylee <dragonflylee@outlook.com>
2024-03-26 13:15:21 +0800

    upgrade dav1d 1.4.1

--- scripts/deb/kylin-v10/deps.sh
+++ scripts/deb/kylin-v10/deps.sh
@@ -6,13 +6,13 @@ set -e
 export PKG_CONFIG_PATH=$CMAKE_PREFIX_PATH/lib/pkgconfig
 export LD_LIBRARY_PATH=$CMAKE_PREFIX_PATH/lib
 
-wget -qO- https://curl.se/download/curl-8.6.0.tar.xz | tar Jxf - -C /tmp
+wget -qO- https://curl.se/download/curl-8.7.1.tar.xz | tar Jxf - -C /tmp
 wget -qO- https://ffmpeg.org/releases/ffmpeg-6.1.1.tar.xz | tar Jxf - -C /tmp
 wget -qO- https://github.com/mpv-player/mpv/archive/v0.36.0.tar.gz | tar zxf - -C /tmp
 wget -qO- https://github.com/webmproject/libwebp/archive/v1.3.2.tar.gz | tar zxf - -C /tmp
 git clone https://github.com/dragonflylee/glfw.git -b switchfin --depth=1 /tmp/glfw
 
-cd /tmp/curl-8.6.0
+cd /tmp/curl-8.7.1
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CMAKE_PREFIX_PATH \
   -DCMAKE_INSTALL_RPATH=$CMAKE_PREFIX_PATH/lib -DBUILD_SHARED_LIBS=ON -DCURL_USE_OPENSSL=ON \
   -DHTTP_ONLY=ON -DCURL_DISABLE_PROGRESS_METER=ON -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF \
--- scripts/deb/ubuntu-focal/deps.sh
+++ scripts/deb/ubuntu-focal/deps.sh
@@ -6,14 +6,14 @@ set -e
 export PKG_CONFIG_PATH=$CMAKE_PREFIX_PATH/lib/pkgconfig
 export LD_LIBRARY_PATH=$CMAKE_PREFIX_PATH/lib
 
-wget -qO- https://curl.se/download/curl-8.6.0.tar.xz | tar Jxf - -C /tmp
-wget -qO- https://downloads.videolan.org/pub/videolan/dav1d/1.4.0/dav1d-1.4.0.tar.xz | tar Jxf - -C /tmp
+wget -qO- https://curl.se/download/curl-8.7.1.tar.xz | tar Jxf - -C /tmp
+wget -qO- https://downloads.videolan.org/pub/videolan/dav1d/1.4.1/dav1d-1.4.1.tar.xz | tar Jxf - -C /tmp
 wget -qO- https://ffmpeg.org/releases/ffmpeg-6.1.1.tar.xz | tar Jxf - -C /tmp
 wget -qO- https://github.com/mpv-player/mpv/archive/v0.36.0.tar.gz | tar zxf - -C /tmp
 git clone https://github.com/dragonflylee/glfw.git -b switchfin --depth=1 /tmp/glfw
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git -b n12.1.14.0 --depth=1 /tmp/nv-codec-headers
 
-cd /tmp/curl-8.6.0
+cd /tmp/curl-8.7.1
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CMAKE_PREFIX_PATH \
   -DCMAKE_INSTALL_RPATH=$CMAKE_PREFIX_PATH/lib -DBUILD_SHARED_LIBS=ON -DCURL_USE_OPENSSL=ON \
   -DHTTP_ONLY=ON -DCURL_DISABLE_PROGRESS_METER=ON -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF \
@@ -21,9 +21,9 @@ cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CMAKE
 cmake --build build
 cmake --install build --strip
 
-cd /tmp/dav1d-1.4.0
+cd /tmp/dav1d-1.4.1
 meson setup build --prefix=$CMAKE_PREFIX_PATH --libdir=lib --buildtype=release --default-library=shared \
-  -Ddebug=false -Denable_tools=false -Denable_examples=false -Denable_tests=false -Denable_docs=false
+  -Denable_tools=false -Denable_examples=false -Denable_tests=false -Denable_docs=false
 meson compile -C build
 meson install -C build
 
@@ -41,7 +41,7 @@ make install
 
 cd /tmp/mpv-0.36.0
 meson setup build --prefix=$CMAKE_PREFIX_PATH --libdir=lib --buildtype=release --default-library=shared \
-  -Dlibmpv=true -Dcplayer=false -Dtests=false -Ddebug=false -Dlibarchive=disabled -Dlua=disabled
+  -Dlibmpv=true -Dcplayer=false -Dtests=false -Dlibarchive=disabled -Dlua=disabled
 meson compile -C build
 meson install -C build
 
