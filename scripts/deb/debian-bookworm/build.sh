3e21a5395c9ca7997ae238695e125307c3c7ebf3
dragonflylee <dragonflylee@outlook.com>
2024-10-10 20:44:50 +0800

    upgrade to mpv 0.39.0

--- scripts/deb/debian-bookworm/build.sh
+++ scripts/deb/debian-bookworm/build.sh
@@ -10,9 +10,10 @@ export LD_LIBRARY_PATH=$CMAKE_PREFIX_PATH/lib
 wget -qO- https://curl.se/download/curl-8.7.1.tar.xz | tar Jxf - -C /tmp
 wget -qO- https://downloads.videolan.org/pub/videolan/dav1d/1.4.3/dav1d-1.4.3.tar.xz | tar Jxf - -C /tmp
 wget -qO- https://ffmpeg.org/releases/ffmpeg-7.1.tar.xz | tar Jxf - -C /tmp
-wget -qO- https://github.com/mpv-player/mpv/archive/v0.36.0.tar.gz | tar zxf - -C /tmp
+wget -qO- https://github.com/mpv-player/mpv/archive/v0.39.0.tar.gz | tar zxf - -C /tmp
 git clone https://github.com/dragonflylee/glfw.git -b switchfin --depth=1 /tmp/glfw
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git -b n12.2.72.0 --depth=1 /tmp/nv-codec-headers
+git clone https://code.videolan.org/videolan/libplacebo.git -b v6.338.2 --depth 1 --recurse-submodules /tmp/libplacebo
 
 cd /tmp/curl-8.7.1
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CMAKE_PREFIX_PATH \
@@ -21,6 +22,12 @@ cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CMAKE
   -DUSE_LIBIDN2=OFF -DCURL_USE_LIBSSH2=OFF -DCURL_USE_LIBPSL=OFF -DBUILD_LIBCURL_DOCS=OFF
 cmake --build build
 cmake --install build --strip
+
+cd /tmp/libplacebo
+meson setup build --prefix=$CMAKE_PREFIX_PATH --libdir=lib --buildtype=release --default-library=shared \
+  -Ddebug=false -Ddemos=false -Dtests=false -Ddovi=enabled -Dlcms=disabled -Dvulkan=disabled
+meson compile -C build
+meson install -C build
 
 cd /tmp/dav1d-1.4.3
 meson setup build --prefix=$CMAKE_PREFIX_PATH --libdir=lib --buildtype=release --default-library=shared \
@@ -42,8 +49,7 @@ cd /tmp/ffmpeg-7.1
 make -j$(nproc)
 make install
 
-cd /tmp/mpv-0.36.0
-patch -Nbp1 -i /opt/scripts/mingw64/mpv/0002-lavfi-channel-layout.patch
+cd /tmp/mpv-0.39.0
 meson setup build --prefix=$CMAKE_PREFIX_PATH --libdir=lib --buildtype=release --default-library=shared \
   -Dlibmpv=true -Dcplayer=false -Dtests=false -Ddebug=false -Dlibarchive=disabled -Dlua=enabled
 meson compile -C build
